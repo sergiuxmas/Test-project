72d2ab1183d9be995ce931eb59ce277059418082
Sergiu Creciun <sergiu.xmas@gmail.com>
2017-05-01 20:39:37 +0300

    Adding class person

---
+++ src/main/java/Person.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IBM on 5/1/2017.
+ */
+public class Person extends EntityTest {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
